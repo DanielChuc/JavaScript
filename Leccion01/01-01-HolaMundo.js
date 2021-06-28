05f6f47f97b218d57603ab7ba0871839bd6d7a5d
DanielChuc <chucdaniel115@gmail.com>
2021-06-28 14:01:56 -0500

    Start (Starto XD)

---
+++ Leccion01/01-01-HolaMundo.js
@@ -0,0 +1,4 @@
+//Variables
+var nombre = "Ubaldo";
+//Imprimirlo en la consola
+console.log(nombre);
---
+++ Leccion01/HolaMundo.js
@@ -0,0 +1,4 @@
+//Variables
+var nombre = "Hola mundo";
+//Imprimirlo en la consola
+console.log(nombre);
